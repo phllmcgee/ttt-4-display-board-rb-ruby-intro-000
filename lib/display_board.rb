4b522d1f8ee12848fd03e2fd55ce02bb1eabf529
Phill McGee <entrenest@outlook.com>
2020-05-27 15:33:09 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,14 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-def display_board = 
-puts  "   |   |   "
-puts  "-----------"
-puts  "   |   |   "
-puts  "-----------"
-puts  "   |   |   "
-end
+def board
 
-board = [" "," "," "," "," "," "," "," "," "]
+puts [" "," "," "," "," "," "," "," "," "]
 
-dislpay_board(board)
